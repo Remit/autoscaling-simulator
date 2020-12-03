4ea94312c32ef5a1ece306b56fee432768d92b15
Remit <v.e.podolskiy@gmail.com>
2020-12-03 21:35:35 +0100

    refactored platform state

--- autoscalingsim/desired_state/platform_state.py
+++ autoscalingsim/desired_state/platform_state.py
@@ -1,33 +1,58 @@
+import collections
+
 from .region import Region
 from .service_group.group_of_services_reg import GroupOfServicesRegionalized
 
 from autoscalingsim.deltarepr.platform_state_delta import PlatformStateDelta
+from autoscalingsim.deltarepr.regional_delta import RegionalDelta
 
 class PlatformState:
 
-    """
-    Wraps the current state of the platform. Structured according to the hierarchy:
+    def __init__(self, regions = None):
 
-    Platform state (1) -> (*) Region (1) -> (1) Node group set (1) -> (*) Homogeneous group (1) -> (*) Entity placement
-    """
+        self.regions = collections.defaultdict(Region)
 
-    def __init__(self, regions = []):
-
-        self.regions = {}
-        if isinstance(regions, dict):
-            for region in regions.values():
-                if not isinstance(region, Region):
-                    raise ValueError(f'An incorrect type of region in the dict provided on init: {region.__class__.__name__}')
-
+        if isinstance(regions, collections.Mapping):
             self.regions = regions
+            
         elif isinstance(regions, list):
             for region_name in regions:
-                if not isinstance(region_name, str):
-                    raise ValueError(f'An incorrect type of region name in the list provided on init: {region_name.__class__.__name__}')
-
                 self.regions[region_name] = Region(region_name)
-        else:
-            raise TypeError(f'Unknown type of regions on init: {regions.__class__.__name__}')
+
+    def __add__(self, state_delta : PlatformStateDelta):
+
+        modified_state = self.copy()
+
+        for region_name, regional_delta in state_delta:
+            if not regional_delta.region_name in modified_state.regions:
+                modified_state.regions[region_name] = Region(region_name)
+            modified_state.regions[region_name] += regional_delta
+
+        return modified_state
+
+    def compute_soft_adjustment(self,
+                                scaling_aspects_adjustment_in_existing_nodes : dict,
+                                scaled_entity_instance_requirements_by_entity : dict) -> tuple:
+        """
+        Attempts to place the service instances in the existing nodes.
+        Returns the deltas of homogeneous groups in regions (or none) and
+        the scaled services remaining unaccommodated to attempt other options.
+        """
+
+        groups_deltas_raw = collections.defaultdict(RegionalDelta)
+        unmet_increase = collections.defaultdict(lambda: collections.defaultdict(dict))
+
+        for region_name, region in self.regions.items():
+            region_groups_delta, region_unmet_scaled_entity_adjustment = region.compute_soft_adjustment(scaling_aspects_adjustment_in_existing_nodes[region_name],
+                                                                                                        scaled_entity_instance_requirements_by_entity)
+            if not region_groups_delta is None:
+                groups_deltas_raw[region_name] = region_groups_delta
+
+            if len(region_unmet_scaled_entity_adjustment) > 0:
+                for service_name, aspects_changes in region_unmet_scaled_entity_adjustment.items():
+                    unmet_increase[region_name][service_name] = {aspect_name: change for aspect_name, change in aspects_changes.items() if change > 0}
+
+        return (PlatformStateDelta(groups_deltas_raw), unmet_increase)
 
     def extract_compensating_deltas(self):
 
@@ -48,22 +73,17 @@ class PlatformState:
 
         return ids_in_regions
 
+    def extract_collective_services_states(self):
+
+        return GroupOfServicesRegionalized({ region_name : region.extract_collective_services_state() for region_name, region in self.regions.items() })
+
     def to_placements(self):
 
         return { region_name : region.to_placement() for region_name, region in self.regions.items()}
 
-    def __add__(self, state_delta : PlatformStateDelta):
+    def to_deltas(self):
 
-        modified_state = self.copy()
-        if not isinstance(state_delta, PlatformStateDelta):
-            raise TypeError(f'An attempt to add an entity of type {state_delta.__class__.__name__} to the {self.__class__.__name__}')
-
-        for region_name, regional_delta in state_delta:
-            if not regional_delta.region_name in modified_state.regions:
-                modified_state.regions[region_name] = Region(region_name)
-            modified_state.regions[region_name] += regional_delta
-
-        return modified_state
+        return PlatformStateDelta([region.to_deltas() for region in self.regions.values()])
 
     def countable_representation(self, conf : dict = {'in-change': True}):
 
@@ -77,15 +97,6 @@ class PlatformState:
 
         return { region_name : region.node_groups_for_change_status(in_change) for region_name, region in self.regions.items() }
 
-    def to_deltas(self):
-
-        """
-        Converts Platform State into PlatformStateDelta by converting corresponding
-        regions to their RegionalDelta representation.
-        """
-
-        return PlatformStateDelta([region.to_deltas() for region in self.regions.values()])
-
     def copy(self):
 
         return self.__class__(self.regions.copy())
@@ -93,40 +104,3 @@ class PlatformState:
     def __repr__(self):
 
         return f'{self.__class__.__name__}( regions = {self.regions} )'
-
-    def compute_soft_adjustment(self,
-                                scaling_aspects_adjustment_in_existing_nodes : dict,
-                                scaled_entity_instance_requirements_by_entity : dict) -> tuple:
-        """
-        Attempts to place the service instances in the existing nodes.
-        Returns the deltas of homogeneous groups in regions (or none) and
-        the scaled services remaining unaccommodated to attempt other options.
-
-        Does not change the state.
-        """
-
-        groups_deltas_raw = {}
-        unmet_scaled_entity_adjustment = {}
-
-        for region_name, region in self.regions.items():
-            region_groups_delta, region_unmet_scaled_entity_adjustment = region.compute_soft_adjustment(scaling_aspects_adjustment_in_existing_nodes[region_name],
-                                                                                                        scaled_entity_instance_requirements_by_entity)
-            if not region_groups_delta is None:
-                groups_deltas_raw[region_name] = region_groups_delta
-
-            if len(region_unmet_scaled_entity_adjustment) > 0:
-                # If we failed to accommodate the negative change in services counts, then
-                # we discard them (no such services to delete, first must add these)
-                unmet_change_positive = {}
-                for service_name, aspects_changes in region_unmet_scaled_entity_adjustment.items():
-                    unmet_change_positive[service_name] = {aspect_name: change for aspect_name, change in aspects_changes.items() if change > 0}
-
-                unmet_scaled_entity_adjustment[region_name] = unmet_change_positive
-
-        state_delta = PlatformStateDelta(groups_deltas_raw)
-
-        return (state_delta, unmet_scaled_entity_adjustment)
-
-    def extract_collective_services_states(self):
-
-        return GroupOfServicesRegionalized({ region_name : region.extract_collective_services_state() for region_name, region in self.regions.items() })
