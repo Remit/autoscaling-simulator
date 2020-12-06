3489720941e0dc61953dd6b1e489d94992c97ccd
Remit <v.e.podolskiy@gmail.com>
2020-12-06 14:00:24 +0100

    refactored specialized placing

--- autoscalingsim/scaling/policiesbuilder/adjustmentplacement/desired_adjustment_calculator/placing/placing_strategy_impl/specialized.py
+++ autoscalingsim/scaling/policiesbuilder/adjustmentplacement/desired_adjustment_calculator/placing/placing_strategy_impl/specialized.py
@@ -1,3 +1,5 @@
+import collections
+
 from autoscalingsim.scaling.policiesbuilder.adjustmentplacement.desired_adjustment_calculator.placing.placing_strategy import PlacingStrategy
 from autoscalingsim.desired_state.placement import InNodePlacement
 from autoscalingsim.desired_state.service_group.group_of_services import GroupOfServices
@@ -11,44 +13,45 @@ class SpecializedPlacingStrategy(PlacingStrategy):
               dynamic_performance = None,
               dynamic_resource_utilization = None):
 
-        placement_options = {}
+        placement_options = dict()
         for provider_name, provider_nodes in placer.node_for_scaled_services_types.items():
             for node_name, node_info in provider_nodes:
-                placement_options_per_node = []
+                placement_options_per_node = list()
 
                 for scaled_service, instance_requirements in placer.scaled_service_instance_requirements_by_service.items():
-                    #current_provider = placer.reader.get_placement_parameter(scaled_service,
-                    #                                                       region_name,
-                    #                                                       'provider')# TODO: think of usage?
 
                     service_state = GroupOfServices(groups_or_aspects = {scaled_service: {'count': 1}}, services_resource_reqs = {scaled_service: instance_requirements})
-                    fits, cap_taken = node_info.services_require_system_resources(service_state)
-                    single_placement_option_instances = {}
+                    single_placement_option_instances = collections.defaultdict(lambda: collections.defaultdict(int))
+                    fits, resources_taken = node_info.services_require_system_resources(service_state)
                     if fits:
-                        cumulative_system_resources = cap_taken
-                        service_instances_count = 1
+                        cumulative_system_resources, service_instances_count = self._attempt_to_place_as_many_service_instances_as_possible_on_the_node(resources_taken)
 
-                        while not cumulative_system_resources.is_full:
-                            cumulative_system_resources += cap_taken
-                            service_instances_count += 1
+                        single_placement_option_instances[scaled_service]['count'] = service_instances_count
 
-                        if cumulative_system_resources.is_full:
-                            cumulative_system_resources -= cap_taken
-                            service_instances_count -= 1
+                        placement_services_state = GroupOfServices(single_placement_option_instances,
+                                                                   {scaled_service: placer.scaled_service_instance_requirements_by_service[scaled_service]})
 
-                        service_placement_option = single_placement_option_instances.get(scaled_service, {})
-                        service_placement_option['count'] = service_instances_count
-                        single_placement_option_instances[scaled_service] = service_placement_option
 
-                        placement_services_state = GroupOfServices(single_placement_option_instances, {scaled_service: placer.scaled_service_instance_requirements_by_service[scaled_service]})
-
-                        single_placement_option = InNodePlacement(node_info,
-                                                                       cumulative_system_resources,
-                                                                       placement_services_state)
-
-                        placement_options_per_node.append(single_placement_option)
+                        placement_options_per_node.append(InNodePlacement(node_info,
+                                                                          cumulative_system_resources,
+                                                                          placement_services_state))
 
                 if len(placement_options_per_node) > 0:
                     placement_options[node_name] = placement_options_per_node
 
         return placement_options
+
+    def _attempt_to_place_as_many_service_instances_as_possible_on_the_node(self, resources_taken):
+
+        cumulative_system_resources = resources_taken
+        service_instances_count = 1
+
+        while not cumulative_system_resources.is_full:
+            cumulative_system_resources += resources_taken
+            service_instances_count += 1
+
+        if cumulative_system_resources.is_full:
+            cumulative_system_resources -= resources_taken
+            service_instances_count -= 1
+
+        return (cumulative_system_resources, service_instances_count)
